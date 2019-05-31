dd3d9bb8eafcdfbea715dac01dad658a6c2d3712
Burkitt123 <51199563+Burkitt123@users.noreply.github.com>
2019-05-31 17:46:06 +0200

    Use github API
    This uses the GIthub API, instead of the browser thing. This requires [PyGithub](https://github.com/PyGithub/PyGithub) to be installed.

--- create.py
+++ create.py
@@ -1,30 +1,18 @@
 import sys
-import os 
-from selenium import webdriver
-
+import os
+from github import Github
 
 path = "/Users/kalle/Documents/Projects/MyProjects/"
-browser = webdriver.Chrome()
-browser.get('http://github.com/login')
 
-username = ''
-password = ''
+username = "" #Insert your github username here
+password = "" #Insert your github password here
 
 def create():
     folderName = str(sys.argv[1])
     os.makedirs(path + str(sys.argv[1]))
-    python_button = browser.find_elements_by_xpath("//input[@name='login']")[0]
-    python_button.send_keys(username) 
-    python_button = browser.find_elements_by_xpath("//input[@name='password']")[0] 
-    python_button.send_keys(password)
-    python_button = browser.find_elements_by_xpath("//input[@name='commit']")[0]
-    python_button.click()
-    browser.get('https://github.com/new') 
-    python_button = browser.find_elements_by_xpath("//input[@name='repository[name]']")[0] 
-    python_button.send_keys(folderName)
-    python_button = browser.find_element_by_css_selector('button.first-in-line')
-    python_button.submit()
-    browser.quit()
+    user = Github(username, password).get_user()
+    repo = user.create_repo(sys.argv[1])
+    print("Succesfully created repository {}".format(sys.argv[1]))
 
 if __name__ == "__main__":
     create()
