8211246e848d04acca6a56c43d82088e669fa57d
Ahmed ElGreetly <aelgreetly@yahoo.com>
2019-06-07 16:01:42 +0300

    Change: sh script todo the job,ADD: Delete project

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
-function create() {
-    cd
-    python create.py $1
-    cd /Users/kalle/Documents/Projects/MyProjects/$1
+PROJECTS_PATH="$HOME/Documents/projects/" # ADD your path
+USERNAME="" # ADD your user name
+PASSWORD="" # ADD your password or leave it blank the console will ask you to enter it
+CREDINTIALS=$USERNAME
+if [[ "$PASSWORD" != "" ]]; then
+    CREDINTIALS="$USERNAME:$PASSWORD"
+fi
+function project(){
+    DELETE_FLAG='false'
+    DELETE_VALUE=''
+    if [[ "$1" == "-d" ]]; then
+        DELETE_FLAG='true'; DELETE_VALUE=$2
+    fi
+    if [ "$DELETE_FLAG" = "true" ]; then
+        cd $PROJECTS_PATH
+        rm -rf $PROJECTS_PATH$DELETE_VALUE
+        GH_CREATE=$(curl -u $CREDINTIALS -X DELETE https://api.github.com/repos/$USERNAME/$DELETE_VALUE)
+        if [[ $GH_CREATE == *"message"* ]]; then
+            echo -e "\033[1;31mMay be there is something wrong please check the curl response."
+            echo $GH_CREATE
+        fi
+        echo -e "\033[1;32mFinished! \033[0m"
+        return;
+    fi
+    mkdir $PROJECTS_PATH$1
+    cd $PROJECTS_PATH$1
+    GH_DELETE=$(curl -u $CREDINTIALS -X POST https://api.github.com/user/repos --data '{"name":"'$1'"}')
+    if [[ "$GH_DELETE" == *"message"* ]]; then
+        echo -e "\033[1;31mMay be there is something wrong please check the curl response."
+        echo $GH_DELETE
+    fi
     git init
-    git remote add origin git@github.com:KalleHallden/$1.git
+    git remote add origin git@github.com:$USERNAME/$1.git
     touch README.md
     git add .
     git commit -m "Initial commit"
     git push -u origin master
     code .
+    echo -e "\033[1;32mFinished! \033[0m"
 }
