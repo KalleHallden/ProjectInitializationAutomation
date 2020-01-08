80ad20ada82462f19d28bbcf65a1cd45dfdb6f47
Aziz Fikri <45133910+afikrim@users.noreply.github.com>
2020-01-09 00:05:52 +0700

    Update .my_commands.sh

--- .my_commands.sh
+++ .my_commands.sh
@@ -3,12 +3,22 @@
 function create() {
     cd
     python create.py $1
-    cd /Users/kalle/Documents/Projects/MyProjects/$1
+    cd ~/Documents/Projects/MyProjects/$1
     git init
-    git remote add origin git@github.com:KalleHallden/$1.git
+    git remote add origin git@github.com:$2/$1.git
     touch README.md
     git add .
     git commit -m "Initial commit"
     git push -u origin master
     code .
+}
+
+function remove() {
+    cd ~/Documents/Projects/MyProjects
+    read -p "Are you sure?[y/n]" confirm
+    if [ "$confirm" = "n" ] || [ "$confirm" = "N" ]; then
+        return
+    fi
+    rm -rf $1
+    python remove.py $1
 }
