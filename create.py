0700023a41df20b97ce6fed38295c6aaa06756c9
Divyanshu Shekhar <61140213+divshekhar@users.noreply.github.com>
2020-02-20 07:40:25 +0530

    ')' Added in line 12.

--- create.py
+++ create.py
@@ -9,7 +9,7 @@ password = "" #Insert your github password here
 
 def create():
     folderName = str(sys.argv[1])
-    os.makedirs(path + str(folderName)
+    os.makedirs(path + str(folderName))
     user = Github(username, password).get_user()
     repo = user.create_repo(folderName)
     print("Succesfully created repository {}".format(folderName))
