0b741a5f570bb9cc0bc1fb728b6f4fd7a87e463d
juampemartin <juamp_m@hotmail.com>
2020-07-10 22:53:05 +0200

    Initial commit

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+
 function create() {
-    cd
-    source .env
-    python create.py $1
+    cd 
+    source .env #Type the path to your .env file here
+    python create.py $1 #Type the path to the create.py path here
     cd $FILEPATH$1
-    git init
-    git remote add origin git@github.com:$USERNAME/$1.git
     touch README.md
+    git init       
     git add .
     git commit -m "Initial commit"
+    git remote add origin git@github.com:$USERNAME/$1.git
     git push -u origin master
-    code .
+    cd ..
+    subl $1
+    cd $1
+    
+}
+
+
+function delete() { 
+    cd
+    source /.env #Type the path to your .env file here     
+    python /delete.py $1 #Type the path to the delete.py path here
+    cd $FILEPATH    
+    rm -r -f $1    
+
 }
