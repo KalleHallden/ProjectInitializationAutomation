8948c84b075691c87ee62fe8bbd5f98bbe127d4a
Red Williams <red.devcs@gmail.com>
2020-07-15 17:53:09 -0400

    Finished choose.py and remote.py
    It works! choose.py correctly passes the arguments to remote.py
    and remote.py correctly pushes to GitHub. Next on the list is to
    integrate local.py.
    
    Also, fixed in issue in remote.py where initializing the
    README.md file didn't create a title, rather plain text, due to a
    syntax error.

--- windows_OS/remote.py
+++ windows_OS/remote.py
@@ -2,55 +2,40 @@ import sys
 import os
 from github import Github
 
-
-if len(sys.argv) < 2 or len(sys.argv) > 3:
-    print("Invalid syntax.\n")
-    
-    print("Usage:")
-    print("  create <repo_name> [options]\n")
-
-    print("Commands:")
-    print("  create\t\t\t\tCreate a repository.")
-    print("  remove\t\t\t\tRemove a repository.")
-    print()
-    print("Optional Parameters:")
-    print("  -h, --help")
-    print("  -d, --description\t\t\t\tAdd a description to the remote repository.")
-    print("  -")
-    sys.exit(1)
-
+# Set variables
 repo_name = str(sys.argv[1])
+repo_description = str(sys.argv[2])
+if str(sys.argv[3]) == "public":
+    repo_publicity = False
+else:
+    repo_publicity = True
 
-try:
-    repo_description = str(sys.argv[2])
-except IndexError:
-    print("No description will be included")
-    repo_description = ""
-sys.exit(0)
-path = "Be sure to add a path"
+
+path = "[Path to workspace here]"
 _dir = path + '\\' + repo_name
 
-token = "Be sure to add a token"
+token = "[GitHub Token here]"
 
 g = Github(token)
 user = g.get_user()
 login = user.login
-repo = user.create_repo(name=repo_name, description=repo_description)
+repo = user.create_repo(name=repo_name, 
+                        description=repo_description,
+                        private=repo_publicity)
 
 os.mkdir(_dir)
 os.chdir(_dir)
 
-commands = [f'echo "# {repo.name}" >> README.md',
+commands = [f'echo # {repo.name} >> README.md',
             'git init',
             f'git remote add origin https://github.com/{login}/{repo.name}.git',
             'git add .',
             'git commit -m "Initial commit"',
             'git push -u origin master']
 
-
-
 for c in commands:
     os.system(c)
 
-print(f'Repository "{repo_name}" created locally')
+print(f'Repository "{repo_name}" created locally and pushed to GitHub!')
+print(f"Check it out at https://github.com/{login}/{repo.name}.git")
 os.system('code .')
