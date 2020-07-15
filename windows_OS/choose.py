71567ec48204869f412d78afeec08a5313812838
Red Williams <red.devcs@gmail.com>
2020-07-15 13:40:16 -0400

    Added logic for help parameter usage
    Next should be usage for the other parameters

--- windows_OS/choose.py
+++ windows_OS/choose.py
@@ -12,28 +12,43 @@ def choose():
         general_usage()
 
     params = {
-        "help" : ["-h", "--help", "help()"],
-        "description" : ["-d", "--description"],
-        "private" : ["-p", "--private"],
-        "local" : ["-l", "--local"],
-        "auto_confirm" : ["-y", "--yes"]
+        "help" :            ["-h", "--help", "help()"],
+        "description" :     ["-d", "--description"],
+        "private" :         ["-p", "--private"],
+        "local" :           ["-l", "--local"],
+        "auto_confirm" :    ["-y", "--yes"]
     }
+
     for i in range(1, len(sys.argv)):
 
         # Test for -h or --help
         if str(sys.argv[i]) in params["help"]:
-            if len(sys.argv) == 2:  # "create -h" prints general usage
-                general_usage()
-            elif len(sys.argv) > 3:    # "create -d -p -h" user doesn't know how to use -h properly
-                help_usage()
-            else:                   # "create --description --help" proper use for option parameter
-                print("Show optional usage")
-                
-                # try:
-                #     # test for regular syntax --> create [option] --help
 
-                # except:
-                #     # test for reversed syntax --> create --help [option]
+            if len(sys.argv) == 2:                              # "create -h" prints general usage
+                general_usage()
+
+            elif len(sys.argv) > 3:                             # "create -d -p -h" user doesn't know how to use -h properly
+                help_usage()
+
+            else:                                               # "create --description --help" proper use for option parameter
+                print("Showing optional usage:\n\n")
+                op_param = str(sys.argv[len(sys.argv) - i]) # len(sys.argv) is 3, checking non help or create param
+
+                if op_param in params["description"]:
+                    print("Showing description usage")
+
+                elif op_param in params["private"]:
+                    print("Showing private usage")
+
+                elif op_param in params["local"]:
+                    print("Showing local usage")
+
+                elif op_param in params["auto_confirm"]:
+                    print("Showing auto confirm usage")
+
+                else:
+                    print("That option is not recognized")
+                    sys.exit(1)
 
             sys.exit(0)
 
@@ -43,6 +58,7 @@ def choose():
             if len(sys.argv) != 3:
                 print("you didn't include a name, showing usage and exiting")
             print("Running local, exiting this program")
+            # TODO Can't exit here, case "create --local help()"
         
         # Test for -d or --description
         if str(sys.argv[i]) in params["description"]:
@@ -86,6 +102,18 @@ def help_usage():
     print()
     print("Description:")
     print("  Explains usage of the \"create\" command and its options")
+
+def description_usage():
+    pass
+
+def private_usage():
+    pass
+
+def local_usage():
+    pass
+
+def auto_confirm_usage():
+    pass
     
 
 
