70956289a21fab262a68a01410256113302e1222
Red Williams <red.devcs@gmail.com>
2020-07-16 02:02:30 -0400

    Finished remove.py
    Still need to work on exiting batch scripts in the right directories. Also need to add usage for remove command

--- windows_OS/remove.bat
+++ windows_OS/remove.bat
@@ -1,1 +1,31 @@
-rem File for remove command when I make it
+@echo off
+
+cd /d %~dp0
+setlocal
+
+rem pyParams variable to contain python script's parameters
+rem Helper flag for parameter presence. default to 0/FALSE.
+set prmOk=0
+
+rem Variable to contain python script name to use. default is remote.
+set pyName=remove
+
+rem Process batch parameters
+:getParam
+
+set pyParams=%pyParams% %1
+set prmOk=1
+
+shift
+rem After SHIFT, %0 is replaced by %1, %1 replaced by %2, %2 replaced by %3, etc.
+if "%~1" neq "" goto getParam
+
+rem Check python script's parameter presence
+if %prmOk% == 0 (
+  echo Batch parameter is missing.
+  goto :eof
+)
+
+python %pyName%.py%pyParams%
+
+:eof
---
+++ windows_OS/remove.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from github import Github
+
+if len(sys.argv) != 2:
+    print("Syntax incorrect")
+    sys.exit(1)
+
+# Log in
+g = Github("b89f916c23a7287dd7b637c90075863cd8c872c3")
+user = g.get_user()
+login = user.login
+
+list = ["-l", "--list"]
+
+def remove():
+        # remove -l, --list
+        if str(sys.argv[1]) in list:
+            pos = 0
+            print()
+            for repo in user.get_repos():
+                pos += 1
+                print(f"  {pos}.   {repo.name}")
+            print()
+                
+            sys.exit(0)
+        else:
+            try:
+                git_url = "https://github.com/"
+                # remove -3
+                if str(sys.argv[1]).startswith("-") and str(sys.argv[1])[1:].isnumeric():
+                    passed_int = int(str(sys.argv[1])[1:])
+                    repo_url =  "{}{}".format(git_url, user.get_repos()[passed_int - 1].full_name)
+                    if delete_repo(repo_url):
+                        user.get_repos()[passed_int - 1].delete()
+                        print(f"Deleted repository {repo_url}")
+                else:
+                # remove repoName
+                    passed_str = str(sys.argv[1])
+                    repo_url = "{}{}".format(git_url, user.get_repo(passed_str).full_name)
+                    if delete_repo(repo_url):
+                        user.get_repo(passed_str).delete()
+                        print(f"Deleted repository {repo_url}")
+            except:
+                print("Error in processing request")
+            print()
+
+def delete_repo(repo_name):
+    for x in range(0, 3): print()
+    print("Warning!")
+    print(f"  Executing this command would remove {repo_name}")
+    print()
+    usr = input("Are you sure you want to delete this repository (y/n)? ")
+    return usr.lower() == "y"
+
+def general_usage()
+    
+if __name__ == "__main__":
+    remove()
